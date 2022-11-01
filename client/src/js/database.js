4e5a8a383fdfec2214adfad0d6416f18db2d0f13
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-11-01 12:44:06 -0400

    added additional code as an alternatve to package.json

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,12 +13,14 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
+// export const putDb = async (id, content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, jate: content });
+  const request = store.put({ jate: content });
+  // const request = store.put({ id: id, jate: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
   console.error('putDb not implemented');
